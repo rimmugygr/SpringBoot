65354bac22e6fd15ed60f8d4600988ee0c404e38
rimmugyr <rimmugygr@gmail.com>
2020-03-23 13:21:53 +0100

    code and project refactor

--- Rest-Api/ClientApplication/src/main/java/client/application/configuration/Application.java
+++ Rest-Api/ClientApplication/src/main/java/client/application/configuration/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"clinet.application"})
+@ComponentScan(basePackages = "client.application")
 public class Application {
 
 	public static void main(String[] args) {
