d0d51f3dc15d84f74adb36a4a80f02eb20000381
rimmugyr <rimmugygr@gmail.com>
2020-03-23 11:20:30 +0100

    add spring seciurity config user login

---
+++ Rest-Api/application/src/main/java/boot/application/configuration/SeciurityConfig.java
@@ -0,0 +1,33 @@
+package boot.application.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SeciurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().hasRole("USER")
+                .and()
+                .formLogin()
+                //.loginPage("/ouerLoginPage")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER");
+    }
+}
